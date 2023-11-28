028c62496a3c010f3f5588b21cee8a7305bff680
troupss <127224895+troupss@users.noreply.github.com>
2023-11-28 12:46:13 +0100

    base de dades feta, afegir coses

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 class database{
     public static function conectar(){
-        $conexio = new mysqli("localhost","root","","biblioteca");//falte afegir server nominalia
+        $conexio = new mysqli("troups.ftp.tb-hosting.com","troupscat@troupscat","","troups_loginphp");
         $conexio->query("SET NAMES 'utf8'");
 
         return $conexio;
